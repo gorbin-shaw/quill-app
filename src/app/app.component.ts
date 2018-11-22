dbc763f22bd2c6f74608cbac84222337df40cedd
Gorbin Philip <gorbin.p@shawacademy.com>
2018-11-22 23:21:04 +0530

    customize editor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation, ViewChild } from '@angular/core';
+
+import Quill from 'quill';
+
+// Add fonts to whitelist
+var Font = Quill.import('formats/font');
+Font.whitelist = ['mirza', 'aref', 'roboto'];
+Quill.register(Font, true);
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: [
+  	'./app.component.css'
+  ],
+  encapsulation: ViewEncapsulation.None
 })
+
 export class AppComponent {
-  title = 'quill-app';
+	title = 'Assignment Editor';
+	ngOnInit() {
+		console.log("ngOnInit");
+	};
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { QuillModule } from 'ngx-quill'
+import { QuillModule } from 'ngx-quill';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
