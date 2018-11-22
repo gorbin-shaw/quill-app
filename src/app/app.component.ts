71121cdefcb2a790c474b6e9e3e0aa21dea99881
Gorbin Philip <gorbin.p@shawacademy.com>
2018-11-23 01:39:29 +0530

    Added editor data binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component, ViewEncapsulation, ViewChild } from '@angular/core';
+import { QuillEditorComponent } from 'ngx-quill';
+import { debounceTime, distinctUntilChanged } from 'rxjs/operators';
 
 import Quill from 'quill';
 
@@ -6,6 +8,10 @@ import Quill from 'quill';
 var Font = Quill.import('formats/font');
 Font.whitelist = ['mirza', 'aref', 'roboto'];
 Quill.register(Font, true);
+
+//import delta
+var Delta = Quill.import('delta');
+var contentDelta = new Delta();
 
 @Component({
   selector: 'app-root',
@@ -17,8 +23,33 @@ Quill.register(Font, true);
 })
 
 export class AppComponent {
+
 	title = 'Assignment Editor';
+	editorContent = "";
+
+	@ViewChild('editor') editor: QuillEditorComponent;
+	
 	ngOnInit() {
-		console.log("ngOnInit");
+		
+		this.editor
+      .onContentChanged
+      .pipe(
+        debounceTime(2000),
+        distinctUntilChanged()
+      )
+      .subscribe(data => {
+        //console.log('Debounced content change', data); //data not relevant
+        //write to storage & clear delta
+        console.log('Updating storage', contentDelta);
+        contentDelta = new Delta();
+      });
+
 	};
+	
+	logChange($event: any) {
+    //console.log('Content changed', $event);
+    //update contentDelta
+    contentDelta = contentDelta.compose($event.delta);
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { QuillModule } from 'ngx-quill';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -12,6 +13,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
     QuillModule
   ],
   providers: [],
